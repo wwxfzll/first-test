902b62d256625cacf5cae1972c09323adc740bae
wwxfzll <276272330@qq.com>
2018-08-21 16:23:26 +0800

    hello

--- build/test.js
+++ build/test.js
@@ -1,7 +1,14 @@
-console.log('test hello github')
+
+console.log('test')
 
 console.log('haha')
 
 function a(){
     console.log('da yin ce shi')
 }
+
+
+let b = ['1', '2']
+b.forEach(function(){
+
+})
